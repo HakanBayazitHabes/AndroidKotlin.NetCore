63a6b3518848f80c8614cedaf5c6c4bb0411996e
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-08 16:52:16 +0300

    Add project files.

---
+++ AndroidKotlin.Auth/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using AndroidKotlin.Auth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace AndroidKotlin.Auth.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize(LocalApi.PolicyName)]
+        public IActionResult Test()
+        {
+            return Ok("Test"); 
+        }   
+
+        public async Task<IActionResult> SignUp(SignUpViewModel signUp)
+        {
+            var user = new ApplicationUser
+            {
+                Email = signUp.Email,
+                UserName = signUp.UserName,
+                City = signUp.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signUp.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            } 
+
+
+
+            return NoContent();
+        }
+    }
+}
---
+++ AndroidKotlin.Auth/Models/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidKotlin.Auth.Models
+{
+    public class SignUpViewModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
