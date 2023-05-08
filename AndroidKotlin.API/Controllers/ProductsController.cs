1dde0845891d3a55d2dd1503478466b0ce54f0ab
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-08 23:11:03 +0300

    Added ProductsController and DeleteProduct() Method

--- AndroidKotlin.API/Controllers/ProductsController.cs
+++ AndroidKotlin.API/Controllers/ProductsController.cs
@@ -48,5 +48,18 @@ namespace AndroidKotlin.API.Controllers
         }
 
 
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct([FromODataUri] int key)
+        {
+            var product = await _context.Products.FindAsync(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
